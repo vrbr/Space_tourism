57bc4ec927119cf86ac44e5be7b7919067e932ff
Мария <lpvamaria@gmail.com>
2021-08-14 11:17:21 +0300

    Закончен блок info, подключен slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+    $('.slider__box').slick({
+        prevArrow: '<img src="images/arrow-left.svg" alt="" class="slider__arrow slider__arrow-left">',
+        nextArrow: '<img src="images/arrow-right.svg" alt="" class="slider__arrow slider__arrow-right">',
+    });
 });
