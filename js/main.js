a12e20a150586aa8c55cdeb2abeeb48e863b4872
Мария <lpvamaria@gmail.com>
2021-08-17 21:02:18 +0300

    Реализован адаптив

--- js/main.js
+++ js/main.js
@@ -2,5 +2,16 @@ $(function(){
     $('.slider__box').slick({
         prevArrow: '<img src="images/arrow-left.svg" alt="" class="slider__arrow slider__arrow-left">',
         nextArrow: '<img src="images/arrow-right.svg" alt="" class="slider__arrow slider__arrow-right">',
+        responsive: [
+            {
+                breakpoint: 481,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
+    $('.menu-btn').on('click', function() {
+        $('.menu__list').toggleClass('active');
+    })
 });
